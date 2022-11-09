edcaeca7240ea152628305bc229ca0d811237175
ShevtsovDenis <shevcov.organika@yandex.ru>
2022-11-09 23:02:40 +0300

    Добавьте файлы проекта.

---
+++ Самостоятельная работа 10. Классы и объекты/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_10.Классы_и_объекты
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение угла: градусы, минуты, секунды");
+            int gradus_ = Convert.ToInt32(Console.ReadLine());
+            int min_ = Convert.ToInt32(Console.ReadLine());
+            int sec_ = Convert.ToInt32(Console.ReadLine());
+            //Создаем экземпляр класса
+            Radian radian = new Radian(gradus_, min_, sec_);
+            //Вычисляем радиан при помощи метода из созданного класса
+            double result_radian = radian.ToRadians();
+            Console.WriteLine("Радиан угла равн {0:f3}",result_radian);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Самостоятельная работа 10. Классы и объекты/Radian.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Самостоятельная_работа_10.Классы_и_объекты
+{
+    class Radian
+    {//Назначаем поля
+        int gradus;
+        int min;
+        int sec;
+        //Назначаем свойства. Проверяем корректность введенных значений.
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+        public int Min
+        { 
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+        public int Sec
+        { 
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+        //Создаем конструктор. Присваиваем полям проверенные значения.
+        public Radian(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        //Создаем метод расчета радиана
+        public double ToRadians()
+        {
+            return gradus * Math.PI / 180 + min * Math.PI / 10800 + sec * Math.PI / 648000;
+        }
+    }
+}
